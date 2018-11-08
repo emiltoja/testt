7b0f09612e59c682abd9e4e05314a1e062c334c8
emiltoja <losemilos3@gmail.com>
2018-11-08 21:07:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -148,7 +148,7 @@ client.on('message', message => {
 
 
     // Purge
-    if (msg.startsWith(prefix + 'czysc')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+    if (msg.startsWith(prefix + 'PURGE')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
         // We have to wrap this in an async since awaits only work in them.
         async function purge() {
             message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
