039adc97584c609331b25288d0856d5826a712ec
emiltoja <losemilos3@gmail.com>
2018-11-08 20:42:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,27 +128,48 @@ client.on('message', message => {
     message.channel.send('twuj stary pijany');
   }
 });
-  if (message.startsWith('/czysc')) {
+
+bot.on('message', message => {
+
+    // Variables - Variables make it easy to call things, since it requires less typing.
+    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
+    let sender = message.author; // This variable takes the message, and finds who the author is.
+    let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+    let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
+
+    // Commands
+
+    // Ping
+    if (msg === prefix + 'PING') { // This checks if msg (the message but in all caps), is the same as the prefix + the command in all caps.
+
+        // Now, let's send a response.
+        message.channel.send('Ping!'); // This 'sends' the message to the channel the message was in. You can change what is in the message to whatever you want.
+
+    }
+
+
+    // Purge
+    if (msg.startsWith(prefix + 'PURGE')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
         // We have to wrap this in an async since awaits only work in them.
         async function purge() {
             message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
 
             // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
             if (!message.member.roles.find("name", "bot-commander")) { // This checks to see if they DONT have it, the "!" inverts the true/false
-                message.channel.send('potrzebujesz \`bot-commander\` roli do uzycia tej komendy.'); // This tells the user in chat that they need the role.
+                message.channel.send('You need the \`bot-commander\` role to use this command.'); // This tells the user in chat that they need the role.
                 return; // this returns the code, so the rest doesn't run.
             }
 
             // We want to check if the argument is a number
             if (isNaN(args[0])) {
                 // Sends a message to the channel.
-                message.channel.send('podaj ilosc wiadomosci do usuniecia. \n uzyj: ' + prefix + 'czysc <ilosc>'); //\n means new line.
+                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>'); //\n means new line.
                 // Cancels out of the script, so the rest doesn't run.
                 return;
             }
 
             const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
-            console.log(fetched.size + ' wiadomosci znalezione, usuwanie . . .'); // Lets post into console how many messages we are deleting
+            console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
 
             // Deleting the messages
             message.channel.bulkDelete(fetched)
@@ -161,6 +182,6 @@ client.on('message', message => {
 
     }
 });
- 
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
