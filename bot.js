a4c1d4becc09ca900924a4f3aec434c8a13ade2b
emiltoja <losemilos3@gmail.com>
2018-11-08 22:59:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,25 @@ client.on('guildMemberAdd', member => {
   channel.send(`witaj na emiltoserwerze, ${member}!`);
 });
 
-client.on ("ready", () => {
-client.user.setGame ("tutaj gra!");
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers | -help`);
+});
+
+client.on("guildCreate", guild => {
+  //  when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers | -help`);
+});
+
+client.on("guildDelete", guild => {
+  // when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers | -help`);
+
 });
 
 //messages 
