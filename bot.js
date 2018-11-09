fdff46bdcca746f1a12511d6cd0bcda2a02b56b8
emiltoja <losemilos3@gmail.com>
2018-11-09 15:04:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -187,18 +187,10 @@ client.on("message", (message) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
     ],
     timestamp: new Date(),
     footer: {
