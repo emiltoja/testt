bd035f4f7f398032cad19e93c5dbaed935ca8b67
emiltoja <losemilos3@gmail.com>
2018-11-09 16:44:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -247,7 +247,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
   // If the message is "how to embed"
-  if (message.content === 'how to embed') {
+  if (message.content === 'test') {
     // We can create embeds using the MessageEmbed constructor
     // Read more about all that you can do with the constructor
     // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
