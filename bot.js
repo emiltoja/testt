378591dd4a6990cfabbe798a19bb9f03ca7517b3
emiltoja <losemilos3@gmail.com>
2018-11-09 16:51:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var Discord = require('discord.js');
-const client = new Discord.Client();
-const RichEmbed = require('discord.js');
+const client = new Discord.Client()
 const prefix = ("/")
 
 client.on('ready', () => {
@@ -244,24 +243,38 @@ client.on('ready', () => {
 client.on('ready', () => {
   console.log('I am ready!');
 });
-
 client.on('message', message => {
-  // If the message is "how to embed"
   if (message.content === 'test') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-    const embed = new RichEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xFF0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
+message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
   }
 });
 
+	  
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
