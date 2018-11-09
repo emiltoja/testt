4e21f350532b8d594263f9a30fd063bad5fbf8c5
emiltoja <losemilos3@gmail.com>
2018-11-09 17:18:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -242,34 +242,28 @@ client.on('ready', () => {
 });
 });
 client.on('message', message => {
-  if (message.content === 'szmusi') {
-message.channel.send({embed: {
+
+  if (message.content.startsWith(prefix + "ping")) {
+	message.channel.send({embed: {
     color: 3447003,
+    color: FF0000,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
+    description: "WOOOPS!! \npodaj ilosc wiadomosci do usuniecia! \n ``` /purge <ilosc>```",
     fields: [{
         name: "Fields",
         value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
+       },
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© Example"
+      text: "© bot by emiltoja"
     }
+  }
+});
   }
 });
 
