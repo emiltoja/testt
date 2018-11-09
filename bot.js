4064d13477063ce84425628518ebe88f3ef0235c
emiltoja <losemilos3@gmail.com>
2018-11-09 13:39:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -189,18 +189,55 @@ client.on('ready', () => {
 
 client.on("message", message => {
 
-    if (message.content === prefix + "test") {
-        let myembed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setAuthor("AUTHOR", message.author.avatarURL)
-        .setFooter("FOOTER", client.user.avatarURL)
-        .setTimestamp(new Date())
-        .setTitle("TITLE")
-        .addField("FIELD 1", "SUBFIELD 1", true)
-        .addField("FIELD 2", "SUBFIELD 2", true)
-        message.channel.send(myembed)
+    if (message.content === prefix + "ssss") {
+        const embed = {
+  "title": "title ~~(did you know you can have markdown here too?)~~",
+  "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+  "url": "https://discordapp.com",
+  "color": 16665999,
+  "timestamp": "2018-11-09T12:37:16.068Z",
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+    "text": "footer text"
+  },
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "image": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "author": {
+    "name": "author name",
+    "url": "https://discordapp.com",
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "fields": [
+    {
+      "name": "🤔",
+      "value": "some of these properties have certain limits..."
+    },
+    {
+      "name": "😱",
+      "value": "try exceeding some of them!"
+    },
+    {
+      "name": "🙄",
+      "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "these last two",
+      "inline": true
+    },
+    {
+      "name": "<:thonkang:219069250692841473>",
+      "value": "are inline fields",
+      "inline": true
     }
-})
+  ]
+};
+channel.send("this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```", { embed 
+});
 
 
 // THIS  MUST  BE  THIS  WAY
