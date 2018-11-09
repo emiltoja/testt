ae21561eec895fc599ca03b9395663a047689d48
emiltoja <losemilos3@gmail.com>
2018-11-09 14:21:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 var Discord = require('discord.js');
-const client = new Discord.Client({disableEveryone: true});
+var client - new Discord.Client();
 var prefix = ("/")
 
 client.on('ready', () => {
