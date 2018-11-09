3c4cfb7109c5d33aa60a3d95ba30e3b39498842a
emiltoja <losemilos3@gmail.com>
2018-11-09 14:09:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -176,6 +176,37 @@ client.on('ready', () => {
     });
 });
 
+client.on('message', message => {
+	  if (message.author.equals(client.user)) return;
+	  
+	  if (!message.content.startWith(prefix)) return;
+	  
+	  var args - message.content.substring(prefix.length).split(" ");
+	  
+	  switch (args[0]. toLowerCase()) {
+		  case "ping":
+			 message.channel.sendMessage("Pong!");
+			 break
+		  case "info":
+		     message.channel.sendMessage("to bot emiltoja");
+			 break;
+		  case "testt":
+		     if (args[1]) message.channe.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+			 else message.channel.sendMessage("testuje");
+			 break;
+		  case "embed":
+		    var embed = new Discord.RichEnbed()
+				.addField("Test Title", "test costam", true)
+				.setColor(0x00FFFFFF)
+			 message.channel.sendEnbed(enbed);
+			 break;
+		  case "noticeme":
+			 message.channel.sendMessage(message.author.torString() + " bitch");
+			 break;
+		  default:
+			 message.channel.sendMessage("Invilad command");
+	  }
+});
 
 
 // THIS  MUST  BE  THIS  WAY
