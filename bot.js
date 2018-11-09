74b59d05aff6ab7fc473b0a5f2befd4b3e04e5db
emiltoja <losemilos3@gmail.com>
2018-11-09 16:01:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,8 +177,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    // If the message is '!rip'
-    if (message.content === '!rip') {
+    // If the message is '/rip'
+    if (message.content === '/rip') {
         // Create the attachment using Attachment
         const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
         // Send the attachment in the message channel
