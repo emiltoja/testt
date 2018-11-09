49862b0bd67aa035cd49a6589a6ccb972c0da8cc
emiltoja <losemilos3@gmail.com>
2018-11-09 15:46:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -178,39 +178,40 @@ client.on('ready', () => {
 
 client.on("message", (message) => {
   // Exit and stop if it's not there
- if (!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(prefix)) return;
  
- if (message.content.startsWith(prefix + "ping")) {
-message.channel.send({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
-});!"
-
-
-
+	if (message.content.startsWith(prefix + "ping")) {
+		message.channel.send({embed: {
+		color: 3447003,
+		author: {
+		name: client.user.username,
+		icon_url: client.user.avatarURL
+		},
+		title: "This is an embed",
+		url: "http://google.com",
+		description: "This is a test embed to showcase what they look like and what they can do.",
+		fields: [{
+			name: "Fields",
+			value: "They can have different fields with small headlines."
+		},
+		{
+			name: "Masked links",
+			value: "You can put [masked links](http://google.com) inside of rich embeds."
+		},
+		{
+			name: "Markdown",
+			value: "You can put all the *usual* **__Markdown__** inside of them."
+		}
+		],
+		timestamp: new Date(),
+		footer: {
+		icon_url: client.user.avatarURL,
+		text: "© Example"
+		}
+		}
+	}
+}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
