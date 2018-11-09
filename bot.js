831a086753a2eb8ca86aaec5a49f40492449ec0a
emiltoja <losemilos3@gmail.com>
2018-11-09 15:01:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -187,6 +187,10 @@ client.on("message", (message) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
         name: "Fields",
         value: "They can have different fields with small headlines."
       },
@@ -198,7 +202,7 @@ client.on("message", (message) => {
         name: "Markdown",
         value: "You can put all the *usual* **__Markdown__** inside of them."
       }
-    
+    ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
