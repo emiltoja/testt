0329445c0da507d110035995139d450de66bfc6b
emiltoja <losemilos3@gmail.com>
2018-11-09 15:11:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -187,10 +187,6 @@ client.on("message", (message) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
         name: "Fields",
         value: "They can have different fields with small headlines."
       },
