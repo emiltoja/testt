9afe0fe9dd8da5433c80bdc6c1ea2eecc3c9a4c1
emiltoja <losemilos3@gmail.com>
2018-11-09 15:00:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -180,20 +180,29 @@ client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
  
-  if (message.content.startsWith(prefix + "szmusi")) {
+  if (message.content.startsWith(prefix + "ping")) {
 	message.channel.send({embed: {
     color: 3447003,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-        name: "szmusi",
+        name: "Fields",
         value: "They can have different fields with small headlines."
-    ],
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© by emiltoja"
+      text: "© Example"
     }
   }
 });
