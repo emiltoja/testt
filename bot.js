b0f9434bd366dc0e0da2230c5cb4fa78675a5a3a
emiltoja <losemilos3@gmail.com>
2019-10-13 22:31:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,85 +1,18 @@
 var Discord = require('discord.js');
 const client = new Discord.Client()
 const api = ("https://emiltoja.pl/lsrp/api.php")
+const snekfetch = require("snekfetch");
+const Discord = require("discord.js");
 const prefix = ("/")
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
+module.exports.run = async (bot, message, args) => {
+	sneakfetch.get(api).then(r => {
+		if (message.content === '/awatar') {
+		  let nickname = r.nickname;
+		  message.channel.send(nickname)
+	});
 
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'emiltoemiltobot');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  channel.send(`witaj na emiltoserwerze :heart: :heart: :heart:, ${member}!`);
-});
-
-client.on('ready', () => {
-    // List servers the bot is connected to
-    console.log("Servers:")
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
-
-        // List all channels
-        guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-        })
-    })
-})
-
-client.on('message', message => {
-  if (!message.guild) return;
-
-  if (message.content.startsWith('/kick')) {
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      const member = message.guild.member(user);
-      if (member) {
-        member.kick('Optional reason that will display in the audit logs').then(() => {
-          message.reply(`uzytkownik ${user.tag} dostal kicka!`);
-        }).catch(err => {
-          message.reply('nie mozesz dac kicka temu uzytkownikowi');
-          console.error(err);
-        });
-      } else {
-        message.reply('uzytkownik nie jest w tej grupie');
-      }
-    } else {
-      message.reply('nieprawidlowy nick lub nie oznaczono za pomoca @ uzytkownika');
-    }
-  }
-});
-
-client.on('ready', () => {
-});
-
-client.on('message', message => {
-  if (!message.guild) return;
-
-  if (message.content.startsWith('/ban')) {
-    const user = message.mentions.users.first();
-    if (user) {
-      const member = message.guild.member(user);
-      if (member) {
-        member.ban({
-          reason: 'byles niegrzeczny kotq!',
-        }).then(() => {
-          message.reply(`uzytkownik ${user.tag} dostal bana`);
-        }).catch(err => {
-          message.reply('nie mozesz dac bana temu uzytkownikowi');
-          console.error(err);
-        });
-      } else {
-        message.reply('uzytkownik nie jest w tej grupie');
-      }
-    } else {
-      message.reply('nieprawidlowy nick lub nie oznaczono za pomoca @ uzytkownika');
-    }
-  }
-});
+}
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -204,50 +137,6 @@ client.on('message', message => {
   }
 });
 
-client.on('message', message => {
-
-    // Variables - Variables make it easy to call things, since it requires less typing.
-    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
-    let sender = message.author; // This variable takes the message, and finds who the author is.
-    let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
-    let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
-
-    // Purge
-    if (msg.startsWith(prefix + 'PURGE')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
-        // We have to wrap this in an async since awaits only work in them.
-        async function purge() {
-            message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
-
-            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
-            if (!message.member.roles.find("name", "emiltomoderator")) { // This checks to see if they DONT have it, the "!" inverts the true/false
-                message.channel.send('musisz posiadac role \`emiltomoderator\` aby uzyc tej komendy'); // This tells the user in chat that they need the role.
-                return; // this returns the code, so the rest doesn't run.
-            }
-
-            // We want to check if the argument is a number
-            if (isNaN(args[0])) {
-                // Sends a message to the channel.
-                message.channel.send('podaj ilosc wiadomosci do usuniecia! \n ``` ' + prefix + 'purge <ilosc>```'); //\n means new line.
-                // Cancels out of the script, so the rest doesn't run.
-                return;
-            }
-
-            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
-            console.log(fetched.size + ' usuwanie wiadomosci...'); // Lets post into console how many messages we are deleting
-
-            // Deleting the messages
-            message.channel.bulkDelete(fetched)
-                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
-
-        }
-
-        // We want to make sure we call the function whenever the purge command is run.
-        purge(); // Make sure this is inside the if(msg.startsWith)
-
-    }
-
-});
-
 client.on('ready', () => {
     client.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
     client.user.setPresence({
@@ -256,63 +145,6 @@ client.on('ready', () => {
             type: 0
         }
     });
-});
-
-client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
- 
-  if (message.content.startsWith(prefix + "ping")) {
-	message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "",
-    url: "",
-    description: "podaj ilosc wiadomosci do usuniecia! \n ``` ' + prefix + 'purge <ilosc>```",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© bot by emiltoja"
-    }
-  }
-});
-  }
-});
-client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
- 
-  if (message.content.startsWith(prefix + "szmusi")) {
-	message.channel.send({embed: {
-    color: 990099,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "",
-    url: "",
-    description: "",
-    fields: [{
-        name: "szmusi",
-        value: "to najwieksza glupia pizda \njesli myslisz ze jest inaczej, to zle myslisz gupku"
-      },
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© bot by emiltoja"
-    }
-  }
-});
-  }
 });
 
 client.on("message", (message) => {
