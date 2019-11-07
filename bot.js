111f0b2aaeb2a6f56662e60ae62381f17b73a073
emiltoja <losemilos3@gmail.com>
2019-11-07 21:50:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,21 +8,12 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', async message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-	
-
-	// ...
+client.on('message', message => {
+  if (command === 'pracownicy') {
+  const { nickname } = await fetch('http://mathelusz.ct8.pl/lsrp/api.php').then(response => response.json());
+    message.channel.send(file);
+  }
 });
-client.on("message", (message) => {
-	if (command === 'pracownicy') {
-	const { nickname } = await fetch('http://mathelusz.ct8.pl/lsrp/api.php').then(response => response.json());
-
-	message.channel.send(file);
-}
 });
 
 client.login(process.env.BOT_TOKEN);
