9c694c4ac5b90eb824a8f3133dd8abc883555622
emiltoja <losemilos3@gmail.com>
2019-11-07 21:48:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,28 @@
 var Discord = require('discord.js');
-const api = "http://mathelusz.ct8.pl/lsrp/api.php";
-const client = new Discord.Client()
-const prefix = ("/")
+const client = new Discord.Client();
+const prefix = '/';
+const fetch = require('node-fetch');
 
-client.on('ready', () => {
-  console.log('I am ready!');
+
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
+client.on('message', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	
+
+	// ...
+});
 client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
- 
-  if (message.content.startsWith(prefix + "pracownicy")) {
-	message.channel.send({embed: {
-    color: 990099,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "",
-    url: "",
-    description: "",
-    fields: [{
-        name: "emil",
-        value: "nickname"
-      },
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© bot by emiltoja"
-    }
-  }
-});
-  }
+	if (command === 'pracownicy') {
+	const { nickname } = await fetch('http://mathelusz.ct8.pl/lsrp/api.php').then(response => response.json());
+
+	message.channel.send(file);
+}
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
